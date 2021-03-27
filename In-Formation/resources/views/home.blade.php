3c409ca8590bc6455afff62474a8154f890f8b99
Puck van Zutphen <rpvzutphen@gmail.com>
2021-03-27 12:48:03 +0100

    IF-002 - create hero card on home

--- In-Formation/resources/views/home.blade.php
+++ In-Formation/resources/views/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-
+  <div class="bg-blue-50 box-content p-12">
+    <div class="w-2/5 mx-auto">
+      <h2 class="text-center font-bold my-2">Geef nu je beschikbaarheid op!</h2>
+      <p class="text-center">Door het invullen van jouw beschikbaarheid, wordt het gemakkelijker om de roosters in te delen en krijgen jouw collega's een goed idee over wanneer jij wel of niet zou kunnen werken</p>
+      <button class="bg-blue-600 py-2 px-8 my-4 rounded-lg text-white flex mx-auto">Klik hier!</button>
+    </div>
+  </div>
 @endsection
--- In-Formation/resources/views/layouts/navbar.blade.php
+++ In-Formation/resources/views/layouts/navbar.blade.php
@@ -1,33 +1,34 @@
 <div class="relative bg-white">
-    <div class="max-w-7xl mx-auto">
-      <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10  px-4 sm:px-6">
-        <div class="flex justify-start lg:w-0 lg:flex-1">
-          <a href="#">
-            <span class="sr-only">Workflow</span>
-            <img class="h-8 w-auto sm:h-10" src="{{URL('/images/favicon.ico')}}" alt="">
-          </a>
-        </div>
-        <div class="-mr-2 -my-2 md:hidden">
-          <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-            <span class="sr-only">Open menu</span>
-            <!-- Heroicon name: outline/menu -->
-            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
-        </div>
-        <nav class="hidden md:flex space-x-10 items-center justify-end md:flex-1 lg:w-0">  
-          <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
-            Rooster
-          </a>
-          <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
-            Collega's
-          </a>
-          <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
-            <div class="flex items-centered">
-                Mijn account
-            </div>
-          </a>
-        </nav>
+  <div class="max-w-7xl mx-auto">
+    <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10  px-4 sm:px-6">
+      <div class="flex justify-start lg:w-0 lg:flex-1">
+        <a href="#">
+          <span class="sr-only">Workflow</span>
+          <img class="h-8 w-auto sm:h-10" src="{{URL('/images/favicon.ico')}}" alt="">
+        </a>
       </div>
+      <div class="-mr-2 -my-2 md:hidden">
+        <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+          <span class="sr-only">Open menu</span>
+          <!-- Heroicon name: outline/menu -->
+          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
+      <nav class="hidden md:flex space-x-10 items-center justify-end md:flex-1 lg:w-0">  
+        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+          Rooster
+        </a>
+        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+          Collega's
+        </a>
+        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+          <div class="flex items-centered">
+              Mijn account
+          </div>
+        </a>
+      </nav>
     </div>
+  </div>
+</div>
